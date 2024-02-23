607c192960cfad66f1412efb1753c323ef7a2739
Maksim-R <rmb89@mail.ru>
2024-02-23 20:57:34 +0500

    add new person in departments

--- CompanyConsole/Program.cs
+++ CompanyConsole/Program.cs
@@ -28,18 +28,59 @@ company.Departments.Add(administration);
 Department it = new Department("IT");
 company.Departments.Add(it);
 
-Department legal = new Department("Юридический");
+Department legal = new Department("Legal");
 company.Departments.Add(legal);
 
-Department accounting = new Department("Бухгалтерия");
+Department accounting = new Department("Accounting");
 company.Departments.Add(accounting);
 
-Department oi = new Department("Охрана труда");
+Department oi = new Department("OI");
 company.Departments.Add(oi);
 
-Employee e1 = new Employee();
-e1.FirstName = "Ivan";
-e1.Email = "qwe@qwe.ru";
-company.Departments[0].Employees?.Add(e1);
+Employee admin1 = new Employee();
+admin1.FirstName = "Иван";
+admin1.LastName = "Фролов";
+admin1.Gender = "Мужской";
+admin1.BirthDate = DateOnly.Parse("11 11.1999");
+admin1.Email = "qwe@test.ru";
+admin1.Phone = "79511234567";
+company.Departments[0].Employees?.Add(admin1);
+
+Employee it1 = new Employee();
+it1.FirstName = "Ирина";
+it1.LastName = "Фурьева";
+it1.Gender = "Женский";
+it1.BirthDate = DateOnly.Parse("11 11.1983");
+it1.Email = "IF@test.ru";
+it1.Phone = "79511234567";
+company.Departments[1].Employees?.Add(it1);
+
+Employee leg1 = new Employee();
+leg1.FirstName = "Ирина";
+leg1.LastName = "Фурьева";
+leg1.Gender = "Женский";
+leg1.BirthDate = DateOnly.Parse("11 11.1983");
+leg1.Email = "IF@test.ru";
+leg1.Phone = "79511234567";
+company.Departments[2].Employees?.Add(leg1);
+
+Employee acc1 = new Employee();
+acc1.FirstName = "Светлана";
+acc1.LastName = "Антипова";
+acc1.Gender = "Женский";
+acc1.BirthDate = DateOnly.Parse("11 11.1969");
+acc1.Email = "SA@test.ru";
+acc1.Phone = "79511234567";
+company.Departments[3].Employees?.Add(acc1);
+
+Employee oi1 = new Employee();
+oi1.FirstName = "Лариса";
+oi1.LastName = "Лобода";
+oi1.Gender = "Женский";
+oi1.BirthDate = DateOnly.Parse("11 11.1965");
+oi1.Email = "LA@test.ru";
+oi1.Phone = "79511234567";
+company.Departments[4].Employees?.Add(oi1);
+
 
 company.PrintDepartmentsInfo();
